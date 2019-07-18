17ee344166e434c27111e6ba9523348bcc257221
VadZim <vadzim11@tut.by>
2019-07-18 15:36:52 +0200

    add rate nbrb in telegramBot

--- Python/py_telegram_bot_api/py_telegram_bot_api.py
+++ Python/py_telegram_bot_api/py_telegram_bot_api.py
@@ -1,13 +1,18 @@
 import telebot
 from my_token import TOKEN
+from rate_nbrb import get_money
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def send_text(message):
     if message.text == "Привет":
-        bot.send_message(message.chat.id, "Привет, мой создатель")
+        bot.send_message(message.chat.id, "Привет!")
     elif message.text == "Пока":
-        bot.send_message(message.chat.id, "Прощай, создатель")
+        bot.send_message(message.chat.id, "Пока!")
+    elif "курс" in message.text:
+        bot.send_message(message.chat.id, get_money(message.text))
+    else :
+        bot.send_message(message.chat.id, "Ты что несешь?")
 
 bot.polling()
---
+++ Python/py_telegram_bot_api/rate_nbrb.py
@@ -0,0 +1,35 @@
+import requests
+
+def get_money(text):
+    s = text.split()
+
+    if len(s)>1:
+        url = f"http://www.nbrb.by/API/ExRates/Rates/{s[1]}?ParamMode=2"
+        r = requests.get(url).json()
+
+        data = ("- " + str(r["Date"])[0:10] + " rate " 
+            + str(r["Cur_Name"]) + " "
+            + str(r["Cur_Scale"]) + " "
+            + str(r["Cur_Abbreviation"])
+            + " - " +  str(r["Cur_OfficialRate"]) 
+            + " BIN")
+        return data
+        
+    else:
+        url = "http://www.nbrb.by/API/ExRates/Rates?Periodicity=0"
+        r = requests.get(url).json()
+        data = ""
+        for i in range(len(r)) :
+            data += ("- " + str(r[i]["Date"])[0:10] + " rate " 
+            + str(r[i]["Cur_Name"]) + " "
+            + str(r[i]["Cur_Scale"]) + " "
+            + str(r[i]["Cur_Abbreviation"])
+            + " - " +  str(r[i]["Cur_OfficialRate"]) 
+            + " BIN\n")
+        return data
+
+def main():
+    get_money("курс")
+
+if __name__ == "__main__":
+    main()
