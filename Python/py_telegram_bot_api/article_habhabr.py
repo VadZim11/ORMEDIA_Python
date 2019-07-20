9f3fc07a78843d002021a14d9ce61823e451776c
VadZim <vadzim11@tut.by>
2019-07-20 21:59:47 +0200

    add keybord

---
+++ Python/py_telegram_bot_api/article_habhabr.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+        
+def get_data(html):
+    soup = BeautifulSoup(html, "lxml")
+    h1 = soup.findAll("h2", {"class": "post__title"})
+    list_name = set()
+    for i in h1:        
+        a = i.find("a").text
+        list_name.add(a) 
+    return list_name
+
+def get_page(html):
+    soup = BeautifulSoup(html, "lxml")
+    h1 = soup.find("a", {"class": "toggle-menu__item-link toggle-menu__item-link_pagination toggle-menu__item-link_bordered"}).get("href")
+    return h1
+
+def main():
+
+    url = "https://habr.com/top/monthly/"
+    finish_page = int(get_page(get_html(url))[-3:-1])
+
+    # print(finish_page)
+
+    list_articles_habr = list()
+
+    for i in range(1, finish_page) :
+        url = "https://habr.com/top/monthly//page" + str(i) + "/"
+        list_articles_habr = list_articles_habr + list(get_data(get_html(url)))
+    
+    #list_articles_habr.sort()
+    print("За месяц вышло " + str(len(list_articles_habr)) + " статей: ")
+
+    for i in range(len(list_articles_habr)):
+        print("- " + str(list_articles_habr[i]))
+
+if __name__ == "__main__":
+    main()
--- Python/py_telegram_bot_api/py_telegram_bot_api.py
+++ Python/py_telegram_bot_api/py_telegram_bot_api.py
@@ -1,18 +1,48 @@
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from my_token import TOKEN
 from rate_nbrb import get_money
 
 bot = telebot.TeleBot(TOKEN)
 
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 3
+    markup.add(InlineKeyboardButton("USD", callback_data="USD"),
+                InlineKeyboardButton("EUR", callback_data="EUR"),
+                InlineKeyboardButton("RUB", callback_data="RUB"),
+                InlineKeyboardButton("ALL", callback_data="ALL"))
+    return markup
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "USD":
+        bot.answer_callback_query(call.id, show_alert=True, text=get_money("курс USD"))
+    elif call.data == "EUR":
+        bot.answer_callback_query(call.id, show_alert=True, text=get_money("курс EUR"))
+    elif call.data == "RUB":
+        bot.answer_callback_query(call.id, show_alert=True, text=get_money("курс RUB"))
+    elif call.data == "ALL":
+        bot.answer_callback_query(call.id, show_alert=True, text=get_money("курс"))
+        
+
 @bot.message_handler(content_types=["text"])
 def send_text(message):
+
+    #print(chat_handler)
+
+    if message.text == "что я писал 5 сообщений назад":
+        bot.send_message(message.chat.id, "Привет!")    
     if message.text == "Привет":
         bot.send_message(message.chat.id, "Привет!")
     elif message.text == "Пока":
         bot.send_message(message.chat.id, "Пока!")
     elif "курс" in message.text:
-        bot.send_message(message.chat.id, get_money(message.text))
+        bot.send_message(message.chat.id, "Выбери валюту", reply_markup = gen_markup())
     else :
         bot.send_message(message.chat.id, "Ты что несешь?")
+
+
+
 
 bot.polling()
